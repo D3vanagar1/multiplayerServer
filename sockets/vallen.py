d447cf7dceb09ea2df888064572e33dfdb066829
Narak <755980@gmail.com>
2022-08-22 00:13:17 +0000

    fixed error

--- sockets/vallen.py
+++ sockets/vallen.py
@@ -6,8 +6,7 @@ PORT = 65432 # port to listen to (special ports > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
   s.connect((HOST, post))
-  s.sendall(b"not chicken nuggies steven is bad boy and he is a really really big big big big big bigbjasjdfjnvak;fjnv;kjsnefavsvfa 
-  food and belly button and i like to eat the refused food and i like to eat more food and the food is really delishiousis and i fancied the food then i shoved it in me mout")
+  s.sendall(b"not chicken nuggies steven is bad boy and he is a really really big big big big big bigbjasjdfjnvak;fjnv;kjsnefavsvfa food and belly button and i like to eat the refused food and i like to eat more food and the food is really delishiousis and i fancied the food then i shoved it in me mout")
   data = s.recv(12345678910111213141516171819202122232425262728293031323334353637383940)
 
 
