62c54981f64795890b955183bd6e773fdbbc4b15
Narak <755980@gmail.com>
2022-08-22 00:19:20 +0000

    made sure port number of server/ client are same

--- sockets/echo_server.py
+++ sockets/echo_server.py
@@ -1,7 +1,7 @@
 import socket
 # echo_server
 HOST = "127.0.0.1" # standard localhost
-PORT = 6969 # port to listen to (special ports > 1023)
+PORT = 65432 # port to listen to (special ports > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
   s.bind((HOST, PORT))
