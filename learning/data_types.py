cbabc76f214b7de4ebcdecbd330e9a9aa4d50fc2
Narak <755980@gmail.com>
2022-09-11 23:30:48 +0000

    introduction to tuples

---
+++ learning/data_types.py
@@ -0,0 +1,32 @@
+# DATA TYPES
+
+# tuples
+x = () #emtpy tuple
+#print(x)
+
+tuplex = tuple() # built in function
+#print(tuplex)
+
+x = ("tuple", False, 3.2, 1)
+#print(x)
+
+# save item from list into variable
+a = [3, 5, "apple"]
+string = a[2] # string = apple
+print(string)
+
+# save item from tuple into variable
+n1, n2, n3, n4 = x
+#print(n3+n4)
+
+#tuplex = n1, n2, n3
+#print(tuplex)
+
+#example: get item from tuple (letter s)
+tuplex = ("w",3,"r","e", " s", "o", "u", "r")
+item= tuplex[-3]
+print(item)
+
+
+#challenge convert tuple into dictionary
+
