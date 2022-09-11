814acf4cc3939c67276ce3d33b7a4cfe09cb7469
Narak <755980@gmail.com>
2022-09-11 22:57:27 +0000

    make sure the server and client receive same data

--- sockets/echo_client.py
+++ sockets/echo_client.py
@@ -8,7 +8,7 @@ PORT = 65432 # port to listen to (special ports > 1023)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
   s.connect((HOST, PORT))
   s.sendall(b"my food is really food and you should eat it")
-  data = s.recv(4)
+  data = s.recv(1024)
 
 
 print(f"Received {data!r}")
