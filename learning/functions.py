ebd7161ea3eca0e76bb9fcab27dc2e99ca30851f
Narak <755980@gmail.com>
2022-09-18 23:21:44 +0000

    functions

---
+++ learning/functions.py
@@ -0,0 +1,18 @@
+# f(a) -> b
+
+def poop(a, b, array):
+    c=0
+    for i in array:
+        c = c + 13*a/b # this will run 5 times
+    return c
+
+
+#print(poop(4,2, [1,2,3,4,5]))
+
+# for loops
+# add all parts of list in for loop
+ll = [5,4,6,9,-1,3]
+
+EEEEEEEEEEED#E GAB DDDDDDDDDDDDDDDDDDDDDDDDD CCCCCCC BBBBBBBBBBBB G# B g#
+
+
