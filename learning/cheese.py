f6da548a1bc465971811119e82608be7dfd5c31e
Narak <755980@gmail.com>
2022-09-18 23:42:59 +0000

    first attempt by vallen and ethan

--- learning/cheese.py
+++ learning/cheese.py
@@ -8,5 +8,11 @@ for i in ll:
 # for loops
 # add all parts of list with a for loop
 ll = [5,4,6,9,-1,3]
-for i in ll:
+for i in 11: 
     print(i)
+
+
+
+
+
+
--- learning/vallen_chickenNuggets.py
+++ learning/vallen_chickenNuggets.py
@@ -8,5 +8,7 @@ for i in ll:
 # for loops
 # add all parts of list with a for loop
 ll = [5,4,6,9,-1,3]
+
 c = 0
-c = c + 1
+for i in ll:
+    print(i)
