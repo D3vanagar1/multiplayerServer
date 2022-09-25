bc66131366350e8261640f5de91f37530d67d098
Narak <755980@gmail.com>
2022-09-25 23:28:51 +0000

    init exercise

---
+++ learning/AsteroidRacer/cheese.py
@@ -0,0 +1,7 @@
+# make an empty class where we set the caption of the mass murder
+
+# gun class
+# self.caption = "Cheese"
+
+class gun:
+    def__init
---
+++ learning/AsteroidRacer/vallen.py
@@ -0,0 +1,4 @@
+# make an empty class where we set the caption of the mass murder
+
+# gun class
+# self.caption = "Cheese"
