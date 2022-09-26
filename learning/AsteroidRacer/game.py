88b4f16c72b70f41cb3045d78cd442f083523b71
Narak <755980@gmail.com>
2022-09-26 00:07:09 +0000

    fixed?

--- learning/AsteroidRacer/game.py
+++ learning/AsteroidRacer/game.py
@@ -1,6 +1,39 @@
+# pyglet doccumentation: https://steveasleep.com/pyglettutorial.html
+
 
 import pyglet
 import random
 
-print("test")
-yea chicken nuggies nope no i dont see
+pyglet.resource.path = ["resources"]
+pyglet.resource.reindex()
+'''
+class AsteroidWindow(pyglet.window.Window):
+    def __init__(self):
+        pass
+
+
+
+asteroidCheese = AsteroidWindow()
+'''
+
+
+# learning about classes
+
+class Rectangle:
+    def __init__(self,length,width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+    
+    def perimeter(self):
+        return 2*(self.length + self.width)
+
+
+class Square(Rectangle):
+    def __init__(self, length):
+        super().__init__(length, width)
+
+square = Square(4)
+print(square.area())
