fceb366587b967e3c9c1ceeafe6c747b68c79c3f
Narak <755980@gmail.com>
2022-09-26 00:21:14 +0000

    Cube problem

--- learning/AsteroidRacer/cheese.py
+++ learning/AsteroidRacer/cheese.py
@@ -14,3 +14,22 @@ ethan2 = gun()
 
 print(ethan.caption)
 print(ethan2.caption)
+
+# task add cube class, has volume function
+
+
+class Rectangle:
+    def __init__(self,length,width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+    
+    def perimeter(self):
+        return 2*(self.length + self.width)
+
+
+class Square(Rectangle):
+    def __init__(self, length):
+        super().__init__(length, length)
--- learning/AsteroidRacer/vallen.py
+++ learning/AsteroidRacer/vallen.py
@@ -13,3 +13,22 @@ vallen2 = mass_muda()
 
 print(vallen.caption)
 print(vallen2.caption)
+
+
+# task add cube class, has volume function
+
+class Rectangle:
+    def __init__(self,length,width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+    
+    def perimeter(self):
+        return 2*(self.length + self.width)
+
+
+class Square(Rectangle):
+    def __init__(self, length):
+        super().__init__(length, length)
