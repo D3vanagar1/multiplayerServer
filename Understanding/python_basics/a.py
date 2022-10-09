2d6e7eb73ec978b924d47332d6886d4ebf525b7d
Narak <755980@gmail.com>
2022-10-09 23:47:49 +0000

    update fixed

---
+++ Understanding/python_basics/a.py
@@ -0,0 +1,7 @@
+# Maximum: find maximum of 2 numbers
+# input: 2 numbers
+# output: 1 number (the larger number)
+def maximum(a,b):
+    # code goes here
+    billy = a>b
+    return billy
--- Understanding/python_basics/functions.py
+++ Understanding/python_basics/functions.py
@@ -10,15 +10,4 @@ def adder1(num1, num2):
     answer = num1 + num2
     return answer
 
-#print(adder1(3,4))
-
-
-
-# Maximum: find maximum of 2 numbers
-# input: 2 numbers
-# output: 1 number (the larger number)
-def maximum(a,b):
-    # code goes here
-    billy = a>b
-    return billy
-chimichang and billy bang willy wang super idol de shang yopjg n dou maei ni de taing ai de yi bai ling wu du de ni diddi de zheng ching chznen liang adhdjdfhgdklkasfhjfhdjlkfhljkfhldjkfhsdjlk ghkjghfjdkhgjfdhgjdhg jrhgj
+print(adder1(3,4))
