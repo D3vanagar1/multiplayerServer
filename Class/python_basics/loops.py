00b27aceb39046ca174390bbb6322688ac478cad
Narak <755980@gmail.com>
2022-11-07 00:10:20 +0000

    first

---
+++ Class/python_basics/loops.py
@@ -0,0 +1,24 @@
+'''
+- for loop
+- while loop
+'''
+
+
+# while loop
+var = 5
+while var > 0:
+    print(var)
+    var = var - 1
+
+
+# infinity loop
+test = True
+i = 0
+while test:
+    print("Help")
+    if i == 3:
+        test = False
+    i += 1
+
+ඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞ = sus
+ඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞ
